97c8030b63306bed1847155939ef807babdb30de
shuuy <84204420+shuuyu@users.noreply.github.com>
2024-07-05 22:20:31 -0700

    Replace publishing to GitHub with sonatype to avoid providing credentials

--- buildSrc/src/main/kotlin/live/shuuyu/scripts/interaktions-publishing.gradle.kts
+++ buildSrc/src/main/kotlin/live/shuuyu/scripts/interaktions-publishing.gradle.kts
@@ -1,5 +1,7 @@
 package live.shuuyu.scripts
 
+import gradle.kotlin.dsl.accessors._6dfd0906a4509b327c85862d9682362c.javadoc
+import gradle.kotlin.dsl.accessors._6dfd0906a4509b327c85862d9682362c.kotlinSourcesJar
 import live.shuuyu.scripts.utils.Project
 
 plugins {
@@ -10,23 +12,66 @@ plugins {
 publishing {
     publications {
         withType<MavenPublication>().configureEach {
+            artifactId = project.name
             groupId = Project.GROUP
             version = Project.VERSION
+
+            pom {
+                name = "Discord InteraKTions"
+                description = "Kotlin Library for Receiving and Handling Discord Interactions via Web Servers/Gateway with Kord"
+                url = Project.URL
+                packaging = "jar"
+
+                scm {
+                    connection.set("scm:git:https://github.com/catgirlclient/DiscordInteraKTions.git")
+                    developerConnection.set("scm:git:https://github.com/catgirlclient/DiscordInteraKTions.git")
+                    url.set(Project.URL)
+                }
+
+                licenses {
+                    license {
+                        name.set("GNU Lesser General Public License Version 3.0")
+                        url.set("https://www.gnu.org/licenses/lgpl-3.0.en.html")
+                    }
+                }
+
+                developers {
+                    developer {
+                        name = "yujin"
+                    }
+                }
+
+                issueManagement {
+                    system = "GitHub"
+                    url = "https://github.com/catgirlclient/DiscordInteraKTions/issues"
+                }
+            }
         }
 
         register(Project.NAME, MavenPublication::class.java) {
             from(components["java"])
+            artifact(tasks.javadoc)
+            artifact(tasks.kotlinSourcesJar)
         }
     }
 
     repositories {
         maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/catgirlclient/DiscordInteraKTions/")
+            name = "Sonatype"
+            url = if(Project.VERSION.endsWith("-SNAPSHOT")) {
+                uri(Project.SONATYPE_SNAPSHOT)
+            } else { uri(Project.SONATYPE_RELEASE) }
             credentials {
                 username = System.getenv("MAVEN_USERNAME")
                 password = System.getenv("MAVEN_PASSWORD")
             }
         }
     }
+}
+
+signing {
+    val signingKey = System.getenv("SIGNING_KEY")
+    val signingPassword = System.getenv("SIGNING_PASSWORD")
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing.publications)
 }
--- buildSrc/src/main/kotlin/live/shuuyu/scripts/utils/Project.kt
+++ buildSrc/src/main/kotlin/live/shuuyu/scripts/utils/Project.kt
@@ -4,4 +4,8 @@ object Project {
     const val NAME: String = "DiscordInteraKTions"
     const val VERSION: String = "0.1.0"
     const val GROUP: String = "live.shuuyu.discordinteraktions"
+    const val URL: String = "https://github.com/catgirlclient/DiscordInteraKTions"
+
+    const val SONATYPE_SNAPSHOT = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+    const val SONATYPE_RELEASE = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
 }
