5ff38ba441cf208d9c80c39bb37cc9a4c262959b
shuuy <hyivines@proton.me>
2024-07-08 12:55:28 -0700

    Bump patch version

--- buildSrc/src/main/kotlin/live/shuuyu/scripts/utils/Project.kt
+++ buildSrc/src/main/kotlin/live/shuuyu/scripts/utils/Project.kt
@@ -2,7 +2,7 @@ package live.shuuyu.scripts.utils
 
 object Project {
     const val NAME: String = "DiscordInteraKTions"
-    const val VERSION: String = "0.1.0"
+    const val VERSION: String = "0.1.1"
     const val GROUP: String = "live.shuuyu.discordinteraktions"
     const val URL: String = "https://github.com/catgirlclient/DiscordInteraKTions"
 
